1d65d2e04e295c29f0ac76c4e8de37fb7063f20a
rishabhmalviya08 <rishabhmalviya08@gmail.com>
2026-04-24 14:21:20 -0700

    feat(payment): integrate stripe

--- demo_microservices/main_payment.py
+++ demo_microservices/main_payment.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Request, HTTPException
 import time
+import requests
 from logger import setup_logger
 
 app = FastAPI()
 logger = setup_logger("PaymentAPI")
+
+def stripe_charge(card_number, amount=100):
+    if card_number == "4242424242424242":
+        logger.warning(f"Processing test card {card_number} via external Stripe API...")
+        res = requests.post("https://httpstat.us/504?sleep=30000", json={"test": True})
+        res.raise_for_status()
+    else:
+        time.sleep(0.5)
 
 @app.post("/payment/process")
 async def process_payment(request: Request):
@@ -19,11 +28,14 @@ async def process_payment(request: Request):
         card_number = user_data.get("card_number")
         
         logger.info(f"Initiating transfer with card {card_number} for address {address}")
-        time.sleep(0.5) 
+        stripe_charge(card_number)
         
         logger.info("Payment processed successfully")
         return {"status": "success", "transaction_id": "txn_succ_123"}
         
+    except requests.RequestException as e:
+        logger.error(f"External payment gateway error: {e}", exc_info=True)
+        raise HTTPException(status_code=502, detail="Bad Gateway")
     except HTTPException:
         raise
     except Exception as e:
